a562eb0384518180b0ac63554ce78660d44335f4
KtVasquezMorales <ktvasquez12@hotmail.com>
2024-07-22 14:13:16 -0500

    Update FileManager.java

--- src/main/java/com/example/demo/solidresolve/FileManager.java
+++ src/main/java/com/example/demo/solidresolve/FileManager.java
@@ -3,6 +3,6 @@ package com.example.demo.solidresolve;
 public class FileManager implements  IFileManager {
     @Override
     public void saveToFile(String name) {
-       System.out.println("Archivo main guardado");
+       System.out.println("Archivo" + name + " guardado");
     }
 }
