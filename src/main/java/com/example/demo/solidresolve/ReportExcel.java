6c3a2bd41853b4f2540ba97f0800e35fb460c775
KtVasquezMorales <ktvasquez12@hotmail.com>
2024-07-23 18:37:27 -0500

    Feature/ajuste kt (#2)
    * Update README.md
    
    * Update EmployeeManager.java
    
    * Update FileManager.java
    
    * Update IReportGenerator.java
    
    * Update ReportExcel.java
    
    * Update ReportPdf.java

--- src/main/java/com/example/demo/solidresolve/ReportExcel.java
+++ src/main/java/com/example/demo/solidresolve/ReportExcel.java
@@ -2,7 +2,7 @@ package com.example.demo.solidresolve;
 
 public class ReportExcel implements IReportGenerator{
     @Override
-    public void generateReport(EmployeeManager employeeManager) {
-        System.out.println("Report generated as excel");
+    public void generateReport(Employee employee) {
+        System.out.println("Report generated as excel para el empleado " + employee.getName());
     }
 }
--- src/main/java/com/example/demo/solidresolve/ReportPdf.java
+++ src/main/java/com/example/demo/solidresolve/ReportPdf.java
@@ -3,7 +3,7 @@ package com.example.demo.solidresolve;
 public class ReportPdf implements IReportGenerator{
 
     @Override
-    public void generateReport(EmployeeManager employeeManager) {
-        System.out.println("Report generated as pdf");
+    public void generateReport(Employee employee) {
+        System.out.println("Report generated as pdf para el empleado " + employee.getName());
     }
 }
